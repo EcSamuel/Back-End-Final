ee21f117918d5fbf8296abef1e5cc8cd47eea296
E C Samuel <158526099+EcSamuel@users.noreply.github.com>
2024-10-07 14:51:46 -0500

    Improved Documentation

--- src/main/java/com/rulezero/playerconnector/controller/model/AvailabilityData.java
+++ src/main/java/com/rulezero/playerconnector/controller/model/AvailabilityData.java
@@ -2,6 +2,7 @@ package com.rulezero.playerconnector.controller.model;
 
 import lombok.Data;
 
+//Column level information for the MySQL Database
 @Data
 public class AvailabilityData {
     private Long availabilityId;
